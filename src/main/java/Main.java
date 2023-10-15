6fc23ea23cc7c736df34de5a2768fa4def4cd9d9
luneva1997 <whatswrongnow@mail.ru>
2023-10-15 13:59:21 +0300

    trying

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+
+        long startTs = System.currentTimeMillis(); // start time
+
+        //Создание списка потоков
+        List<Thread> threads = new ArrayList<>();
+
+        for (String text : texts) {
+            //Создаем поток и реализуем логику
+            Runnable r;
+
+            threads.add(new Thread(r = () -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            }
+            ));
+        }
+
+        int max = 0;
+
+        //Запуск потоков
+        for (Thread thread: threads){
+            thread.start();
+        }
+
+        for (Thread thread: threads){
+            thread.join();
+        }
+
+        long endTs = System.currentTimeMillis(); // end time
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
 }
